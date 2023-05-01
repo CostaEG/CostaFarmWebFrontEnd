081402de5b15c079298d7c2909efb456cf11c391
DevCostafarms <131572229+DevCostafarms@users.noreply.github.com>
2023-05-01 12:52:17 -0400

    Initial changes
    Initial changes

--- features/src/core/manifest.ts
+++ features/src/core/manifest.ts
@@ -12,11 +12,11 @@ export let manifest: Manifest = {
     title: "HS Plaform",
     authMode: "oauth 2.0",
     //apiUrl: "http://10.0.0.180:3001",
-    apiUrl: "http://192.168.86.55:5263",
-    authUrl: "https://login.microsoftonline.com/81b3e404-f0eb-4596-9cbb-1768eaa0bbb4/oauth2/v2.0/authorize",
-    tokenUrl: "https://login.microsoftonline.com/81b3e404-f0eb-4596-9cbb-1768eaa0bbb4/oauth2/v2.0/token",
-    clientId: "d747a0c2-0a2c-4d7e-9165-d38027958541",
-    scopes: ["offline_access", "api://d747a0c2-0a2c-4d7e-9165-d38027958541/app"]
+    apiUrl: "http://192.168.1.239:5263",
+    authUrl: "https://login.microsoftonline.com/e284194e-3218-4557-be77-6643231d35b1/oauth2/v2.0/authorize",
+    tokenUrl: "https://login.microsoftonline.com/e284194e-3218-4557-be77-6643231d35b1/oauth2/v2.0/token",
+    clientId: "1894a6d1-f5f5-40b0-acdc-a219976f91a0",
+    scopes: ["offline_access", "api://1894a6d1-f5f5-40b0-acdc-a219976f91a0/webaccess"]
 };
 
 switch(process.env['NODE_ENV']) {
